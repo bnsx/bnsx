dda1b25a7df8063551d9784618627542a1237c3b
Ammarin Boonsong <bnsdev@mail.com>
2023-03-07 21:39:16 +0700

    add font for all page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import { IBM_Plex_Sans_Thai } from "next/font/google";
+const font = IBM_Plex_Sans_Thai({
+  subsets: ["latin", "thai"],
+  weight: ["100", "200", "300", "500", "700"],
+});
+
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={font.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
