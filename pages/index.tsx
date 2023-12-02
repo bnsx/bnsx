5b7222bb343cd7334fa9acc6aa731aa8d2d4f290
Ammarin Boonsong <ammarin.bns@gmail.com>
2023-12-02 10:38:44 +0700

    update about layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,11 +9,11 @@ export default function Home(): JSX.Element {
       <Head>
         <title>My Profile</title>
       </Head>
-      <div className="w-full h-screen flex justify-center items-center">
-        <div className="bg-white drop-shadow-xl rounded-xl w-72 h-[500px] overflow-hidden p-10">
-          <div className="mt-5">
+      <div className="w-full h-screen flex justify-center items-center px-5">
+        <div className="bg-white drop-shadow-xl rounded-xl xl:w-72 h-[500px] overflow-hidden p-8 space-y-1">
+          <div className="pt-2.5">
             <Image
-              className="transform hover:scale-150 duration-300 w-40 h-40 rounded-full mx-auto cursor-pointer"
+              className="transform hover:scale-150 object-contain duration-300 w-40 rounded-md mx-auto cursor-pointer"
               src={"/profile.jpg"}
               width={2000}
               height={2000}
@@ -26,7 +26,7 @@ export default function Home(): JSX.Element {
           </div>
           <div className="text-center">
             <p className="text-gray-500">My hobbies</p>
-            <p className="text-lg">Create application and read a book.</p>
+            <p className="text-lg">Create application and invest in myself.</p>
           </div>
           <div className="flex justify-center">
             <FaceBook_Link url="https://facebook.com/blackbird.no" />
