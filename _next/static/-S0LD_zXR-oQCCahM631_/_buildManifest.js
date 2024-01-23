2c0de56d1e45b43a6127877aaa7bb5ede1da4f8f
bnsx <bnsx@users.noreply.github.com>
2024-01-23 03:12:48 +0000

    deploy: 5a19d2f73d9820636ef5529f2dba8d5a1b41c6a8

---
+++ _next/static/-S0LD_zXR-oQCCahM631_/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/247-eeb9d3d8d9e3320d.js","static/chunks/pages/index-8993afe6118c7135.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
