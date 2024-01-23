8527b057e89c6a828f4747a0a896651013a9a8c0
Ammarin <ammarin.bns@gmail.com>
2024-01-23 10:01:08 +0700

    added output : `export`

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
+const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
+  output: "export",
   reactStrictMode: true,
-  assetPrefix: isProd ? '/bnsx/' : '',
-}
+  assetPrefix: isProd ? "/bnsx/" : "",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
