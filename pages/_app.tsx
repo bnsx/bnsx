6c29f1ad9b2e7a92533b99f69f32b1b0ef471a58
Ammarin <ammarin.bns@gmail.com>
2024-01-24 09:12:29 +0700

    removed react-query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,14 @@
 import "@/styles/globals.css";
 import { IBM_Plex_Sans_Thai } from "next/font/google";
-import { QueryClient, QueryClientProvider } from "react-query";
 const font = IBM_Plex_Sans_Thai({
   subsets: ["latin", "thai"],
   weight: ["100", "200", "300", "500", "700"],
 });
 
-const queryClient = new QueryClient();
-
 export default function App({ Component, pageProps }: any) {
   return (
     <main className={font.className}>
-      <QueryClientProvider client={queryClient}>
-        <Component {...pageProps} />
-      </QueryClientProvider>
+      <Component {...pageProps} />
     </main>
   );
 }
