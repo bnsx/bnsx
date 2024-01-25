d2d89c1b61874d3c112c783dbe0cd5436008f73b
Ammarin <ammarin.bns@gmail.com>
2024-01-25 09:23:39 +0700

    removed `assetPrefix`

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === "production";
+// const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
   output: "export",
   images: { unoptimized: true },
   reactStrictMode: true,
-  assetPrefix: isProd ? "/bnsx" : "",
+  // assetPrefix: isProd ? "/bnsx" : "",
 };
 
 module.exports = nextConfig;
